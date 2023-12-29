994dc7976623b9158e384dd0f220635db680717c
Tyna William <tynawilliam@gmail.com>
2023-12-29 11:34:43 -0400

    add additional routes and style 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SettingsPage from "./pages/SettingsPage";
+import Library from "./pages/Library";
+import Favorites from "./pages/Favorites";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
   );
