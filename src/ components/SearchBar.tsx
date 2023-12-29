1c986630e81ec85b70b9788cc4743c376db3b8a5
Tyna William <tynawilliam@gmail.com>
2023-12-28 21:47:17 -0400

    mobile responsiveness

--- src/ components/SearchBar.tsx
+++ src/ components/SearchBar.tsx
@@ -6,7 +6,7 @@ export default function SearchBar() {
       <input
         type="text"
         placeholder="Search"
-        className=" w-1/2 h-10 my-5 pl-3 bg-blue-100 placeholder-zinc-500"
+        className="hidden md:block w-1/2 h-10 my-5 pl-3 bg-blue-100 placeholder-zinc-500"
       />
     </>
   );
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -11,16 +11,21 @@ export default function HomePage() {
   return (
     <div className={`h-screen flex flex-col`}>
       <div className={`main_container flex h-[90vh]`}>
-        <div className={`left_panel w-1/5 h-full border flex flex-col`}>
+        <div className="left_panel hidden md:block md:w-1/5 h-full border flex flex-col">
           <LeftPanel />
         </div>
-        <div className={`center_panel w-3/5 border flex flex-col px-12`}>
+        <div
+          className={`center_panel w-full md:w-3/5 border flex flex-col px-4 md:px-12`}
+        >
           <SearchBar />
           <Carousel />
           <RecommendedSongs />
           <RecommendedArtists />
+          {/* <div className="md:hidden">
+            <RightPanel />
+          </div> */}
         </div>
-        <div className={`right_panel w-1/5 border`}>
+        <div className="right_panel hidden md:block w-1/5 border">
           <RightPanel />
         </div>
       </div>
