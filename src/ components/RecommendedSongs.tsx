54b7f4046f5fd4ad2a42f4b31063005399aeab4b
Tyna William <tynawilliam@gmail.com>
2023-12-28 20:13:15 -0400

    create main page layout

---
+++ src/ components/RecommendedSongs.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RecommendedSongs() {
+  return <div>RecommendedSongs</div>;
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,32 @@
 import React from "react";
+import LeftPanel from "../ components/LeftPanel";
+import RightPanel from "../ components/RightPanel";
+import SearchBar from "../ components/SearchBar";
+import { FeaturedMusicCarousel } from "../ components/FeaturedMusicCarousel";
+import RecommendedSongs from "../ components/RecommendedSongs";
+import RecommendedArtists from "../ components/RecommendedArtists";
+import AudioPlayer from "../ components/AudioPlayer";
 
 export default function HomePage() {
   return (
-    <>
-      <div className="main_container">
-        Home
-        <div className="left_panel"></div>
-        <div className="center_panel"></div>
-        <div className="right_panel"></div>
+    <div className={`h-screen flex flex-col`}>
+      <div className={`main_container flex h-[90vh]`}>
+        <div className={`left_panel w-1/5 h-full border flex flex-col`}>
+          <LeftPanel />
+        </div>
+        <div className={`center_panel w-3/5 border`}>
+          <SearchBar />
+          <FeaturedMusicCarousel />
+          <RecommendedSongs />
+          <RecommendedArtists />
+        </div>
+        <div className={`right_panel w-1/5 border`}>
+          <RightPanel />
+        </div>
       </div>
-      <div className="audio_player"></div>
-    </>
+      <div className={`audio_player h-[10vh]`}>
+        <AudioPlayer />
+      </div>
+    </div>
   );
 }
