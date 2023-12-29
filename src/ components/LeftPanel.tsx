166ed84efebab74952b404b5f5bb89939c099496
Tyna William <tynawilliam@gmail.com>
2023-12-28 20:28:43 -0400

    left panel layout

--- src/ components/LeftPanel.tsx
+++ src/ components/LeftPanel.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 
 export default function LeftPanel() {
-  return <div>LeftPanel</div>;
+  return (
+    <div className="flex flex-col h-full items-center bg-blue-100">
+      <h1 className="h-1/6 w-4/5">LeftPanel</h1>
+      <div className="h-1/6 w-4/5">
+        <ul className="">
+          <li>Home</li>
+          <li>Search</li>
+          <li>Library</li>
+          <li>Favorites</li>
+        </ul>
+      </div>
+      <div className="h-1/6 w-4/5">
+        <h3>Playlists</h3>
+        <ul>
+          <li>Pop</li>
+          <li>Rock</li>
+          <li>Country</li>
+          <li>Classical</li>
+          <li>R&B</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
